c1da5d28f8e9c54b0431f64fc4f23d6c7ac8f8ed
MarynaKarat <maryna_karatysheuskaya@epam.com>
2026-04-16 18:52:33 +0300

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 60_000,
+  expect: { timeout: 15_000 },
+  retries: 1,
+  use: {
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
